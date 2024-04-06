8ce233bf59f9cc2ef4bc6cc4b9d61cb7395af031
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-04-07 00:06:27 +0900

    Initial Commit

---
+++ AppStore-Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore-Search
+//
+//  Created by 조유진 on 4/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
